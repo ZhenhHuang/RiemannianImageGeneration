8bdabb5bccca9ed9d4e8ffa5a8a4245b43bbef60
RiemanGraph <huangzhenhao_work@163.com>
2023-10-17 12:41:12 +0800

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -9,13 +9,17 @@ import matplotlib.pyplot as plt
 def visualize(model, labels, dev_str='cuda:0', figsize=(30, 30), save_path=None):
     images = model.generate(labels, dev_str).permute(0, 2, 3, 1) * 0.5 + 0.5   # (B, W, H, C)
     images = images.detach().cpu().numpy()
+    if images.shape[-1] == 1:
+        cmap = 'gray'
+    else:
+        cmap = None
     plt.figure(figsize=figsize)
     num = len(labels)
     row = int(np.sqrt(num)) + 1
     for i in range(num):
         ax = plt.subplot(row, row, i+1)
         ax.set_title(f"Class: {labels[i].item()}")
-        plt.imshow(images[i])
+        plt.imshow(images[i].clip(0, 1), cmap=cmap)
     if save_path is not None:
         plt.savefig(save_path)
 
